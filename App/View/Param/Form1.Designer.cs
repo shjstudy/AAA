75d25cbd184b9da3eec5bb47e0ffba00a9fda3c5
chenyf999 <5018900@163.com>
2017-11-13 22:39:07 +0800

    Signed-off-by: chenyf999 <5018900@163.com>

--- App/View/Param/Form1.Designer.cs
+++ App/View/Param/Form1.Designer.cs
@@ -50,10 +50,9 @@ namespace App.View.Param
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.button1.Location = new System.Drawing.Point(22, 48);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(33, 72);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 26);
+            this.button1.Size = new System.Drawing.Size(168, 39);
             this.button1.TabIndex = 0;
             this.button1.Text = "更新库存消息";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,10 +61,9 @@ namespace App.View.Param
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.button2.Location = new System.Drawing.Point(22, 124);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(33, 186);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 26);
+            this.button2.Size = new System.Drawing.Size(168, 39);
             this.button2.TabIndex = 1;
             this.button2.Text = "堆垛机状态";
             this.button2.UseVisualStyleBackColor = true;
@@ -74,9 +72,10 @@ namespace App.View.Param
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.button3.Location = new System.Drawing.Point(23, 195);
+            this.button3.Location = new System.Drawing.Point(34, 292);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 27);
+            this.button3.Size = new System.Drawing.Size(168, 40);
             this.button3.TabIndex = 2;
             this.button3.Text = "堆垛机报警信息";
             this.button3.UseVisualStyleBackColor = true;
@@ -85,40 +84,45 @@ namespace App.View.Param
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox3.Location = new System.Drawing.Point(22, 157);
+            this.textBox3.Location = new System.Drawing.Point(33, 236);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox3.Size = new System.Drawing.Size(839, 37);
+            this.textBox3.Size = new System.Drawing.Size(1256, 54);
             this.textBox3.TabIndex = 5;
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox1.Location = new System.Drawing.Point(22, 7);
+            this.textBox1.Location = new System.Drawing.Point(33, 10);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(839, 40);
+            this.textBox1.Size = new System.Drawing.Size(1256, 58);
             this.textBox1.TabIndex = 6;
             this.textBox1.Text = "[{\"Operation\":\"minus\", \"ID\":\"2\", \"Color\":\"light\",\"Type\":\"lid\",\"Number\":\"3\" }]";
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox2.Location = new System.Drawing.Point(22, 83);
+            this.textBox2.Location = new System.Drawing.Point(33, 124);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(839, 40);
+            this.textBox2.Size = new System.Drawing.Size(1256, 58);
             this.textBox2.TabIndex = 7;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.txtResult);
-            this.groupBox1.Location = new System.Drawing.Point(867, -1);
+            this.groupBox1.Location = new System.Drawing.Point(1300, -2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 385);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(300, 578);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "反馈结果";
@@ -126,83 +130,90 @@ namespace App.View.Param
             // txtResult
             // 
             this.txtResult.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.txtResult.Location = new System.Drawing.Point(6, 20);
+            this.txtResult.Location = new System.Drawing.Point(9, 30);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(4);
             this.txtResult.Multiline = true;
             this.txtResult.Name = "txtResult";
             this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtResult.Size = new System.Drawing.Size(188, 325);
+            this.txtResult.Size = new System.Drawing.Size(280, 486);
             this.txtResult.TabIndex = 6;
             // 
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox4.Location = new System.Drawing.Point(22, 232);
+            this.textBox4.Location = new System.Drawing.Point(33, 348);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox4.Size = new System.Drawing.Size(839, 40);
+            this.textBox4.Size = new System.Drawing.Size(1256, 58);
             this.textBox4.TabIndex = 12;
             // 
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox5.Location = new System.Drawing.Point(22, 307);
+            this.textBox5.Location = new System.Drawing.Point(33, 460);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
             this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox5.Size = new System.Drawing.Size(839, 37);
+            this.textBox5.Size = new System.Drawing.Size(1256, 54);
             this.textBox5.TabIndex = 11;
             // 
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.button5.Location = new System.Drawing.Point(22, 345);
+            this.button5.Location = new System.Drawing.Point(33, 518);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(112, 27);
+            this.button5.Size = new System.Drawing.Size(168, 40);
             this.button5.TabIndex = 10;
             this.button5.Text = "AGV报警信息";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.button4.Location = new System.Drawing.Point(22, 273);
-            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Location = new System.Drawing.Point(33, 410);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(112, 24);
+            this.button4.Size = new System.Drawing.Size(168, 36);
             this.button4.TabIndex = 9;
             this.button4.Text = "AGV状态";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox6.Location = new System.Drawing.Point(22, 382);
+            this.textBox6.Location = new System.Drawing.Point(33, 573);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
             this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
             this.textBox6.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox6.Size = new System.Drawing.Size(839, 37);
+            this.textBox6.Size = new System.Drawing.Size(1256, 54);
             this.textBox6.TabIndex = 14;
             this.textBox6.Visible = false;
             // 
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.button6.Location = new System.Drawing.Point(22, 422);
+            this.button6.Location = new System.Drawing.Point(33, 633);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(112, 27);
+            this.button6.Size = new System.Drawing.Size(168, 40);
             this.button6.TabIndex = 13;
             this.button6.Text = "补货任务反馈";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
             this.button7.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.button7.Location = new System.Drawing.Point(1114, 11);
-            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Location = new System.Drawing.Point(1671, 16);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(112, 32);
+            this.button7.Size = new System.Drawing.Size(168, 48);
             this.button7.TabIndex = 15;
             this.button7.Text = "关闭";
             this.button7.UseVisualStyleBackColor = true;
@@ -210,9 +221,9 @@ namespace App.View.Param
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1255, 458);
+            this.ClientSize = new System.Drawing.Size(1882, 687);
             this.ControlBox = false;
             this.Controls.Add(this.button7);
             this.Controls.Add(this.textBox6);
@@ -228,7 +239,7 @@ namespace App.View.Param
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3);
             this.Name = "Form1";
             this.Text = "接口测试";
             this.Shown += new System.EventHandler(this.Form1_Shown);
